b13ba1e7733cae4480b9ff7f65f56ab78d06ba94
hindu-1234 <gangahinduvathik@gmail.com>
2024-12-24 11:54:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function addTask(){
+    const taskInput=document.getElementById("new-task");
+    const taskList=document.getElementById("task-list");
+
+     const taskText=taskInput.value.trim();
+
+     const listItem=document.createElement("li");
+
+     const taskSpan=document.createElement("span");
+     taskSpan.textContent=taskText;
+
+     const deleteButton=document.createElement("button");
+     deleteButton.textContent="Delete";
+     deleteButton.style.marginLeft="10px";
+
+     deleteButton.addEventListener("click",()=>{
+        taskList.removeChild(listItem);
+     });
+
+     listItem.appendChild(taskSpan);
+     listItem.appendChild(deleteButton);
+
+     taskList.appendChild(listItem);
+
+     taskInput.Value="";
+}
+
